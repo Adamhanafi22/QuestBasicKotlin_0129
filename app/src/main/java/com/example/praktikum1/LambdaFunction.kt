692f233d67685850dde8b43f07633bd5a6a074ca
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-10-12 22:29:47 +0700

    Lamda function uppercase

--- app/src/main/java/com/example/praktikum1/LambdaFunction.kt
+++ app/src/main/java/com/example/praktikum1/LambdaFunction.kt
@@ -4,3 +4,7 @@ package com.example.praktikum1
 // Lambda function adalah fungsi yang tidak memiliki nama
 // Lambda function biasanya digunakan untuk membuat fungsi yang sederhana
 // lambda function menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String{
+    return string.uppercase()
+}
