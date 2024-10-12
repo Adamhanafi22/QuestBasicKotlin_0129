0645716e0f693dd1ec24529ca44c7a9b455f1f1d
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-10-12 22:27:45 +0700

    Lamda function

---
+++ app/src/main/java/com/example/praktikum1/LambdaFunction.kt
@@ -0,0 +1,6 @@
+package com.example.praktikum1
+
+// Lambda Functions
+// Lambda function adalah fungsi yang tidak memiliki nama
+// Lambda function biasanya digunakan untuk membuat fungsi yang sederhana
+// lambda function menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
