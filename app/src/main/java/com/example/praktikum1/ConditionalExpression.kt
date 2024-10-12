1ea0083da6445e2cb6a4ab35ca2fda904a74ba23
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-10-12 21:49:43 +0700

    menulis hari

--- app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
+++ app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
@@ -2,4 +2,26 @@ package com.example.praktikum1
 
 fun ConditionalStatment(){
     println("=== Conditional Expression ===")
+
+    // if-Else
+    val number = 10
+    if (number > 0){
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+    // when
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+
+    }
 }
